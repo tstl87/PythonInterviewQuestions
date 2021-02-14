7c711d96bb7d949223018b57ceaa3b92fc056296
Skylar Trigueiro <Skylar.Trigueiro@gmail.com>
2021-02-14 13:12:24 -0800

    successfully redid the find first and last instance of target in sorted list problem.

--- binarySearch/findFirstLastPositionElementSortedArray/Solution.py
+++ binarySearch/findFirstLastPositionElementSortedArray/Solution.py
@@ -1,56 +1,98 @@
 class Solution:
   def getRange(self, arr, target):
-    first = self.binarySearch(arr, 0, len(arr) - 1, target, True)
-    last = self.binarySearch(arr, 0, len(arr) - 1, target, False)
 
-    #first = self.binarySearchIterative(arr, 0, len(arr) - 1, target, True)
-    #last = self.binarySearchIterative(arr, 0, len(arr) - 1, target, False)
+    #first = self.binarySearch(arr, target, 0, len(arr)-1, True)
+    #last = self.binarySearch(arr, target, 0, len(arr)-1, False)
+
+    first = self.binarySearchIterative(arr, target, 0, len(arr)-1, True)
+    last = self.binarySearchIterative(arr, target, 0, len(arr)-1, False)
+
     return [first, last]
 
-  def binarySearch(self, arr, low, high, target, findFirst):
+  def binarySearch(self, arr, target, low, high, findFirst):
     if high < low:
       return -1
+    
+    # update mid point using
+    # better version of (high + low ) / 2
     mid = low + (high - low) // 2
+    
+    # find first occurrence of target in the array
     if findFirst:
-      if (mid == 0 or target > arr[mid - 1]) and arr[mid] == target:
+      # if mid becomes one of the end points of the array.
+      if mid == 0 or mid == len(arr) -1:
         return mid
-      if target > arr[mid]:
-        return self.binarySearch(arr, mid + 1, high, target, findFirst)
-      else:
-        return self.binarySearch(arr, low, mid - 1, target, findFirst)
+      
+      # if first occurence is found
+      if arr[mid] == target:
+        if arr[mid -1] < target:
+          return mid
+        else:
+          return self.binarySearch(arr, target, low, mid-1, True)
+      # arr[mid] < target, search again in [mid+1, high] 
+      elif arr[mid] < target:
+        return self.binarySearch(arr, target, mid+1, high, True)
+      # arr[mid] > target, search again in [low, mid-1]
+      elif arr[mid] > target:
+        return self.binarySearch(arr, target, low, mid-1, True)
+    
+    # find the last occurence of target in the array
     else:
-      if (mid == len(arr)-1 or target < arr[mid + 1]) and arr[mid] == target:
+      # if mid becomes one of the end points of the array.
+      if mid == 0 or mid == len(arr) -1:
         return mid
-      elif target < arr[mid]:
-        return self.binarySearch(arr, low, mid - 1, target, findFirst)
-      else:
-        return self.binarySearch(arr, mid + 1, high, target, findFirst)
+      
+      # if last occurence is found
+      if arr[mid] == target:
+        if arr[mid+1] > target:
+          return mid
+        else:
+          return self.binarySearch(arr, target, mid+1, high, False)
+      elif arr[mid] < target:
+        return self.binarySearch(arr, target, mid+1, high, False)
+      elif arr[mid] > target:
+        return self.binarySearch(arr, target, low, mid-1, False)
 
-  def binarySearchIterative(self, arr, low, high, target, findFirst):
+  def binarySearchIterative(self, arr, target, low, high, findFirst):
     while True:
+      # stop if high becomes less than low
       if high < low:
         return -1
+      
       mid = low + (high - low) // 2
+      # stop if mid becomes an endpoint
       if findFirst:
-        if (mid == 0 or target > arr[mid - 1]) and arr[mid] == target:
+        if mid == 0 or mid == len(arr)-1:
           return mid
-        if target > arr[mid]:
-          low = mid + 1
-        else:
-          high = mid - 1
+        if arr[mid] == target:
+          if arr[mid-1] < target:
+            return mid
+          else:
+            high = mid-1
+        elif arr[mid] < target:
+          low = mid+1
+        elif arr[mid] > target:
+          high = mid-1
+
       else:
-        if (mid == len(arr)-1 or target < arr[mid + 1]) and arr[mid] == target:
+        if mid == 0 or mid == len(arr)-1:
           return mid
-        elif target < arr[mid]:
-          high = mid - 1
-        else:
-          low = mid + 1
+        if arr[mid] == target:
+          if arr[mid+1] > target:
+            return mid
+          else:
+            low = mid+1
+        elif arr[mid] < target:
+          low = mid+1
+        elif arr[mid] > target:
+          high = mid-1
     
 
 
-arr = [1, 3, 3, 5, 7, 8, 9, 9, 9, 15]
+
+arr = [0, 1, 3, 3, 5, 7, 8, 9, 9, 9, 15]
 print(Solution().getRange(arr, 9))
-# [6, 8]
+# [7, 9]
 
 # Iterative Solution:
 # Time Complexity:
